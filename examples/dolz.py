2b7de09284daa91c8c8eb05c8a1eda2f05357442
Yt-trium <cyril.meyer.68@gmail.com>
2018-06-05 15:58:57 +0200

    work on dolz model implementation

--- examples/dolz.py
+++ examples/dolz.py
@@ -3,8 +3,8 @@
 # file : examples/dolz.py
 # author : CM
 # Example of training on Bullitt dataset using Jose Dolz FCNN
-# 3D fully convolutional networks for subcortical
-# segmentation in MRI: A large-scale study
+# Original article : 3D fully convolutional networks
+# for subcortical segmentation in MRI: A large-scale study
 #
 # ------------------------------------------------------------ #
 import os
@@ -15,17 +15,25 @@ from utils.config.read import readConfig
 
 from keras import backend as K
 
+# Set ordering : tensorflow (channel last)
 K.set_image_dim_ordering("tf")
 
+# Check config file
 config_filename = sys.argv[1]
 if(not os.path.isfile(config_filename)):
     sys.exit(1)
-
+# Load config
 config = readConfig(config_filename)
 
+# Generate model
+print("Generate model")
 model = dolz_1(config["patch_size_x"],config["patch_size_y"],config["patch_size_z"], 2)
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['categorical_accuracy'])
 
-print(model.input_shape)
-print(model.output_shape)
+# Print model informations
 model.summary()
+print("Input shape", model.input_shape)
+print("Output shape",model.output_shape)
+
+# Load dataset
+print("Loading training dataset")
