b914e8f418c90780ab0e78b43ae23c682a438fcf
Yt-trium <cyril.meyer.68@gmail.com>
2018-06-06 16:31:29 +0200

    improvement in test for dolz

--- utils/learning/patch/reconstruction.py
+++ utils/learning/patch/reconstruction.py
@@ -40,12 +40,17 @@ def dolzReconstruction(image,patchs):
 
     count = 0
 
-    for x in range(0,image.shape[0], patchs.shape[1]):
-        for y in range(0, image.shape[1], patchs.shape[2]):
-            for z in range(0,image.shape[2], patchs.shape[3]):
-                patch = np.argmax(patchs[count], axis=2)
-                patch.reshape(16, 16, 16)
-                output[x:x+patchs.shape[1],y:y+patchs.shape[2],z:z+patchs.shape[3]] = patch
+    print("image shape", image.shape)
+    print("patchs shape", patchs.shape)
+
+    # todo : change 16 with patch shape
+
+    for x in range(0,image.shape[0], 16):
+        for y in range(0, image.shape[1], 16):
+            for z in range(0,image.shape[2], 16):
+                patch = np.argmax(patchs[count], axis=1)
+                patch = patch.reshape(16, 16, 16)
+                output[x:x+patch.shape[0],y:y+patch.shape[1],z:z+patch.shape[2]] = patch
                 count += 1
 
     return output
