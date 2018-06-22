85a15fb5b9f02160f8e01b0c07f987c55990e2cd
Yt-trium <cyril.meyer.68@gmail.com>
2018-06-22 16:18:12 +0200

    add function for intensity normsalition, projection and clipping

--- utils/preprocessing/normalisation.py
+++ utils/preprocessing/normalisation.py
@@ -2,11 +2,28 @@
 #
 # file : preprocessing/normalisation.py
 # author : CM
+#
+# ------------------------------------------------------------ #
+import numpy as np
+
 # Preprocess dataset with intensity normalisation
 # (zero mean and unit variance)
-# ------------------------------------------------------------ #
-
 def intensityNormalisation(dataset, dtype):
     mean = dataset.mean()
     std  = dataset.std()
     return ((dataset - mean) / std).astype(dtype)
+
+# Intensities normalized to the range [0, 1]
+def intensityNormalisationFeatureScaling(dataset, dtype):
+    max = dataset.max()
+    min = dataset.min()
+
+    return ((dataset - min) / (max - min)).astype(dtype)
+
+# Intensity max clipping with c "max value"
+def intensityMaxClipping(dataset, c, dtype):
+    return np.clip(a=dataset, a_max=c).astype(dtype)
+
+# Intensity projection
+def intensityProjection(dataset, p, dtype):
+    return (dataset ** p).astype(dtype)
