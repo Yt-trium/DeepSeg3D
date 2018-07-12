f6f94271e27f92bd374934b0933de1a8b755e55a
Yt-trium <cyril.meyer.68@gmail.com>
2018-07-12 11:52:47 +0200

    update log analyzer

--- tools/log_analyzer.py
+++ tools/log_analyzer.py
@@ -21,6 +21,10 @@ train_loss = []
 valid_loss = []
 train_precision = []
 valid_precision = []
+train_sensitivity = []
+valid_sensitivity = []
+train_specificity = []
+valid_specificity = []
 
 with open(input_filename, 'r') as csvfile:
     log = csv.DictReader(csvfile, delimiter=',')
@@ -33,16 +37,22 @@ with open(input_filename, 'r') as csvfile:
         train_precision.append(float(row["precision"]))
         valid_precision.append(float(row["val_precision"]))
 
+        train_sensitivity.append(float(row["sensitivity"]))
+        valid_sensitivity.append(float(row["val_sensitivity"]))
+
+        train_specificity.append(float(row["specificity"]))
+        valid_specificity.append(float(row["val_specificity"]))
+
 fig = pyplot.figure()
 
-axs0 = fig.add_subplot(2, 1, 1)
+axs0 = fig.add_subplot(3, 1, 1)
 
 axs0.plot(epoch, train_loss, label='train loss')
 axs0.plot(epoch, valid_loss, label='valid loss')
 axs0.set_ylabel('loss')
 pyplot.legend()
 
-axs1 = fig.add_subplot(2, 1, 2)
+axs1 = fig.add_subplot(3, 1, 3)
 
 axs1.plot(epoch, train_precision, label='train precision')
 axs1.plot(epoch, valid_precision, label='valid precision')
@@ -50,4 +60,17 @@ axs1.set_ylabel('precision')
 pyplot.legend()
 
 pyplot.xlabel('epoch')
+
+axs2 = fig.add_subplot(3, 2, 3)
+
+axs2.plot(epoch, train_sensitivity, label='train sensitivity')
+axs2.plot(epoch, valid_sensitivity, label='valid sensitivity')
+axs2.set_ylabel('sensitivity')
+
+axs3 = fig.add_subplot(3, 2, 4)
+
+axs3.plot(epoch, train_specificity, label='train specificity')
+axs3.plot(epoch, valid_specificity, label='valid specificity')
+axs3.set_ylabel('specificity')
+
 pyplot.show()
